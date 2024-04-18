65b08dd5895822095d7823567ed68ff09b84826d
mingrove.dev <hello.mingrove@gmail.com>
2024-04-18 14:45:15 +0900

    Initial Commit

---
+++ test.mingrove/test_mingroveApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_mingroveApp.swift
+//  test.mingrove
+//
+//  Created by LDCC on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct test_mingroveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
